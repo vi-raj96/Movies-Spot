3b9d327bab51fd9d68444b01271b81e1da189eb6
Vijay Rajak <vijayrajak891@gmail.com>
2021-08-25 12:53:30 +0530

    Added all UI

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <div className="nav"> 
+                    <input/>
+                    <button id="search-btn">Search</button>
+            </div> 
+            );
+    }
+}
+
+export default Navbar;
