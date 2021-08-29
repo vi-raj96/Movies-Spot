86f085c349472ae1b6129b2c2319ac1541e6da27
Vijay Rajak <vijayrajak891@gmail.com>
2021-08-29 13:49:52 +0530

    Complete

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { Component } from 'react';
+import { addToFavourites, removeFromFavourites } from '../actions';
 
-class MovieCard extends React.Component {
-    render() {
-        const { movie } = this.props;
-        return (
-            <div className="movie-car">
-                <div className="left">
-                   <img alt="movie-poster" src={movie.Poster}/> 
-                </div>
-                <div className="right"> 
-                    <div className="title">{movie.Title}</div>
-                    <div className="plot">{movie.Plot}</div>
-                    <div className="footer">
-                        <div className="rating">{movie.imdbRating}</div>
-                        <button className="favourite-btn">Favourite</button>
-                    </div>
-                </div>
-            </div>  
-            );
-    }
+class MovieCard extends Component {
+  handleFavouriteClick = () => {
+    const { movie } = this.props;
+    this.props.dispatch(addToFavourites(movie));
+  };
+
+  handleUnFavouriteClick = () => {
+    const { movie } = this.props;
+    this.props.dispatch(removeFromFavourites(movie));
+  };
+
+  render() {
+    const { movie, isFavourite } = this.props;
+    return (
+      <div className="movie-card">
+        <div className="left">
+          <img src={movie.Poster} alt="movie-pic" />
+        </div>
+        <div className="right">
+          <div className="title">
+            {movie.Title} ({movie.Year})
+          </div>
+          <div className="plot">{movie.Plot}</div>
+          <div className="footer">
+            <div className="rating">Rating: {movie.imdbRating}</div>
+            {isFavourite ? (
+              <button
+                className="unfavourite-btn"
+                onClick={this.handleUnFavouriteClick}
+              >
+                Unfavourite
+              </button>
+            ) : (
+              <button
+                className="favourite-btn"
+                onClick={this.handleFavouriteClick}
+              >
+                Favourite
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MovieCard;
- 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,104 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import rootReducer from './reducers';
+import './index.css';
+
+// const logger = function({ dispatch, getState }) {
+//   return function(next) {
+//     return function(action) {
+//       // my middlware
+//       console.log('ACTION', action);
+//       next(action);
+//     };
+//   };
+// };
+
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+  // my middlware
+  console.log('ACTION', action);
+  next(action);
+};
+
+// const thunk = store => next => action => {
+//   if (typeof action === 'function') {
+//     return action(store.dispatch);
+//   }
+
+//   next(action);
+// };
+
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+// // console.log(store);
+// console.log('state', store.getState());
+
+// export const StoreContext = createContext();
+
+// console.log('StoreContext', StoreContext);
+
+// class Provider extends React.Component {
+//   render() {
+//     const { store } = this.props;
+//     return (
+//       <StoreContext.Provider value={store}>
+//         {this.props.children}
+//       </StoreContext.Provider>
+//     );
+//   }
+// }
+
+// const connectedComponent = connect(callback)(App);
+// export function connect(callback) {
+//   return function (Component) {
+//     class ConnectedComponent extends React.Component {
+//       constructor(props) {
+//         super(props);
+//         this.unsubscribe = this.props.store.subscribe(() => {
+//           this.forceUpdate();
+//         });
+//       }
+
+//       componentWillUnmount() {
+//         this.unsubscribe();
+//       }
+//       render() {
+//         const { store } = this.props;
+//         const state = store.getState();
+//         const dataToBeSentAsProps = callback(state);
+
+//         return <Component dispatch={store.dispatch} {...dataToBeSentAsProps} />;
+//       }
+//     }
+
+//     class ConnectedComponentWrapper extends React.Component {
+//       render() {
+//         return (
+//           <StoreContext.Consumer>
+//             {(store) => {
+//               return <ConnectedComponent store={store} />;
+//             }}
+//           </StoreContext.Consumer>
+//         );
+//       }
+//     }
+//     return ConnectedComponentWrapper;
+//   };
+// }
+
+// update store by dispatching actions
+// store.dispatch({
+//   type: 'ADD_MOVIES',
+//   movies: moviesList
+// });
+// console.log('state', store.getState());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
